5de77a2c689084756719f116b527ea7d8a728d34
toru tateyama <feet-10@hotmail.co.jp>
2018-04-06 15:08:39 +0900

    データベース編集

--- db/migrate/20180404075320_create_groups.rb
+++ db/migrate/20180404075320_create_groups.rb
@@ -1,7 +1,7 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-      t.string :name ,null: false, index: true, forget_key: true
+      t.string :name ,null: false, index: true
 
       t.timestamps
     end
