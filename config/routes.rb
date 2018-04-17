b8c014a41d37a11dee1626af09a0d65bcc17fed6
toru tateyama <feet-10@hotmail.co.jp>
2018-04-17 21:07:49 +0900

    Blank line ＆ Indent modify

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,4 @@ Rails.application.routes.draw do
     resources :messages, only: [:create, :index]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 end
