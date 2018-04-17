cc0962226096efa5fbd58f536ec865d7637fdce2
toru tateyama <feet-10@hotmail.co.jp>
2018-04-17 16:15:41 +0900

    messages_controller index add respond

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,27 +2,31 @@ class MessagesController < ApplicationController
   before_action :set_group
   def index
     @message = Message.new
+    respond_to do |format|
+    format.html
+    format.json{ @new_messages = @messages.where('id > ?', params[:message][:id])}
     @messages = @group.messages.includes(:user)
   end
+  end
+
   def create
     @message = @group.messages.new(message_params)
     if @message.save
       respond_to do |format|
-     format.json
-     format.html{ redirect_to group_messages_path(@group) }
-    end
+      format.json
+      format.html{ redirect_to group_messages_path(@group) }
+      end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = "メッセージを入力してください"
       render :index
     end
-
-
   end
+
   private
 
   def message_params
-params.require(:message).permit(:body,:image).merge(user_id: current_user.id)
+    params.require(:message).permit(:body,:image).merge(user_id: current_user.id)
   end
   def set_group
     @group = Group.find(params[:group_id])
